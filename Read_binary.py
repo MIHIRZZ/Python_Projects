a94685d8e9fdcacbdb81593ed0d543c9c08a6876
Mihir Patel <32467154+MIHIRZZ@users.noreply.github.com>
2022-03-13 12:31:42 -0400

    Create Read_binary.py
    This file can be used to read and store binary data like image, audio, and video files.

---
+++ Read_binary.py
@@ -0,0 +1,25 @@
+# following program is to read very small binary files like Image
+"""
+import urllib.request, urllib.parse, urllib.error
+
+img = urllib.request.urlopen('http://data.pr4e.org/cover3.jpg').read()
+fhand = open('cover3.jpg', 'wb')  # wb => is for writing binary file
+fhand.write(img) 
+fhand.close()
+"""
+
+
+# if the binary files are having more binary data like audio or video file then we can use follwing program
+import urllib.request, urllib.parse, urllib.error
+
+img = urllib.request.urlopen('http://data.pr4e.org/cover3.jpg')
+fhand = open('cover3.jpg', 'wb')
+size = 0
+while True:
+    info = img.read(100000)
+    if len(info) < 1: break
+    size = size + len(info)
+    fhand.write(info)
+
+print(size, 'characters copied.')
+fhand.close()
